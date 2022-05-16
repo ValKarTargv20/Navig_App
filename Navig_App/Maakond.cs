fcfdbddfc6d6499f666b6c634e2268d559a4260e
Валерия <@DESKTOP-89Q1D6N>
2022-05-16 11:39:19 +0300

    Add project files.

---
+++ Navig_App/Maakond.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Navig_App
+{
+    public class Maakond : INotifyPropertyChanged
+    {
+        string nimetus;
+        string pealinn;
+        int inimeste_arv;
+
+        public string Nimetus
+        {
+            get { return nimetus; }
+            set
+            {
+                if (nimetus != value)
+                {
+                    nimetus = value;
+                    OnPropertyChanged("Nimetus");
+                }
+            }
+        }
+
+        public string Pealinn
+        {
+            get { return pealinn; }
+            set
+            {
+                if (pealinn != value)
+                {
+                    pealinn = value;
+                    OnPropertyChanged("Pealinn");
+                }
+            }
+        }
+
+        public int Inimeste_arv
+        {
+            get { return inimeste_arv; }
+            set
+            {
+                if (inimeste_arv != value)
+                {
+                    inimeste_arv = value;
+                    OnPropertyChanged("Inimiste_arv");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string v="")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+        }
+
+    }
+}
---
+++ Navig_App/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Navig_App
+{
+    public partial class MainPage : ContentPage
+    {
+        protected internal ObservableCollection<Maakond> Maakonnads { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Maakonnads = new ObservableCollection<Maakond>
+            {
+                new Maakond {Nimetus="Harjumaa", Pealinn="Tallinn", Inimeste_arv=520000},
+                new Maakond {Nimetus= "Tartumaa", Pealinn="Tartu", Inimeste_arv = 320000},
+                new Maakond {Nimetus = "Ida-Virumaa", Pealinn="Kohla-Jarve", Inimeste_arv= 150000}
+            };
+            list.BindingContext = Maakonnads;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Maakonnad(null));
+        }
+
+        private async void list_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Maakond selected = e.SelectedItem as Maakond;
+            if (selected != null)
+            {
+                list.SelectedItem = null;
+                await Navigation.PushAsync(new Maakonnad(selected));
+            }
+        }
+
+        protected internal void AddMaakond(Maakond maakond)
+        {
+            Maakonnads.Add(maakond);
+        }
+    }
+}
